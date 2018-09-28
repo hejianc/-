36172a4e45906ccc8972ceb854291b1eaddf0c74
hejianc <412146263@qq.com>
2018-09-28 09:03:00 +0800

    'update'

--- conf/default.py
+++ conf/default.py
@@ -31,9 +31,9 @@ from django.conf.global_settings import *  # noqa
 # ==============================================================================
 # 在蓝鲸智云开发者中心 -> 点击应用ID -> 基本信息 中获取 APP_ID 和 APP_TOKEN 的值
 APP_ID = 'jamece-framework'
-APP_TOKEN = '0ad0c4d1-b311-45fb-a865-2244d4ced83f'
+APP_TOKEN = '92a14a9b-04d3-4924-90dd-7cade7ea6e89'
 # 蓝鲸智云开发者中心的域名，形如：http://paas.bking.com
-BK_PAAS_HOST = 'http://paas.blueking.com/'
+BK_PAAS_HOST = 'http://paas.bk.com/'
 
 # 是否启用celery任务
 IS_USE_CELERY = True
